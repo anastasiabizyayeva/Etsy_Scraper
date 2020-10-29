431130616678763e0967149657538d6f166d741d
Anastasia Bizyayeva <70596760+anastasiabizyayeva@users.noreply.github.com>
2020-10-29 15:38:29 +0000

    scraped hidden code

--- main_scraper.py
+++ main_scraper.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sun Oct 25 16:24:40 2020
-
 @author: Anastasia
 """
 
@@ -43,193 +42,54 @@ num = 0
 titles = []
 shop_names = []
 is_ad = []
+star_ratings = []
+num_reviews = []
+prices = []
 
 #Loop through the scraping code until we get 6000 records
 
-while record_counter < 10:
+# while record_counter < 10:
     
     #Ensure main search results populate before further action is taken
 
-    try:
-        main = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.ID, 'content')))
-        
-        #Get the listing containers and loop through them
-        
-        results = main.find_elements_by_xpath('//li[starts-with(@class, "wt-list-unstyled wt-grid__item-xs-6 wt-grid__item-md-4 wt-grid__item")]')
-                
-        for result in results: 
-            
-            title = result.find_element_by_css_selector("div > a[href]").get_attribute("title")
-            titles.append(title)
-        
-            shop_name = result.find_element_by_css_selector("p.screen-reader-only").text
-            if shop_name[:2] == 'Ad':
-                is_ad.append(1)
-            else:
-                is_ad.append(0)
-            shop_names.append(shop_name.split(" ")[-1])
-            
-        
-        print(shop_names)
-        print(is_ad)
-        print(titles)
-                
-        #Loop over each vacancy 
+try:
+    main = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, 'content')))
     
-        # for result in results:
-                    
-        #     #Get title
+    #Get the listing containers and loop through them
+    
+    results = main.find_elements_by_xpath('//li[starts-with(@class, "wt-list-unstyled wt-grid__item-xs-6 wt-grid__item-md-4 wt-grid__item")]')[:65]
+    
+    print(len(results))
             
-        #     title = result.find_element_by_css_selector("h2 > a[href]").get_attribute("title")
-        #     titles.append(title)  
-            
-        #     #Get department
-            
-        #     department = result.find_element_by_tag_name("h3").text
-        #     departments.append(department)
-            
-        #     #Get salary info 
-            
-        #     salary = result.find_element_by_class_name('salary')
-        #     salaries.append(salary.text)
-            
-        #     #Get location info
-                   
-        #     employer = result.find_element_by_class_name('agency')
-        #     employers.append(employer.text)
+    for result in results: 
         
-        # #Initialize an empty list to hold links to each job search result 
+        title = result.find_element_by_css_selector("div > a[href]").get_attribute("title")
+        titles.append(title)
+    
+        shop_name = result.find_element_by_css_selector("p.screen-reader-only").text
+        if shop_name[:2] == 'Ad':
+            is_ad.append(1)
+        else:
+            is_ad.append(0)
+        shop_names.append(shop_name.split(" ")[-1])
         
-        # link_list = []
+        star_rating = result.find_element_by_css_selector("span.screen-reader-only").text
+        star_ratings.append(star_rating.split(" ")[0])
         
-        # #Find links for each job posted and append them to our links list 
+        num_review = result.find_element_by_css_selector('span.text-body-smaller.text-gray-lighter.display-inline-block.icon-b-1').text
+        num_reviews.append(num_review.strip("()"))
         
-        # links = driver.find_elements_by_css_selector("h2 > a[href]")
-        
-        # for link in links:
-        #     link_text = link.get_attribute("href")
-        #     link_list.append(link_text)
-        
-        # #Loop over links and get pertinent information
-            
-        # for link in link_list:
-        #     driver.get(link)
-            
-        #     #Get job type 
-            
-        #     j_type = driver.find_elements_by_xpath("//dt[.='Job Type:']/following-sibling::dd[1]")
-        #     if not j_type: x = "None"
-        #     else: x = j_type[0].text
-        #     job_types.append(x)
-            
-        #     #Get working pattern
-            
-        #     w_pattern = driver.find_elements_by_xpath("//dt[.='Working pattern:']/following-sibling::dd[1]")
-        #     if not w_pattern: x = "None"
-        #     else: x = w_pattern[0].text
-        #     working_patterns.append(x)
-            
-        #     #Get pay scheme
-            
-        #     p_scheme = driver.find_elements_by_xpath("//dt[.='Pay Scheme:']/following-sibling::dd[1]")
-        #     if not p_scheme: x = "None"
-        #     else: x = p_scheme[0].text
-        #     pay_schemes.append(x)
-            
-        #     #Get pay band
-            
-        #     p_band = driver.find_elements_by_xpath("//dt[.='Pay Band:']/following-sibling::dd[1]")
-        #     if not p_band: x = "None"
-        #     else: x = p_band[0].text
-        #     pay_bands.append(x)
-             
-        #     #Get location
-            
-        #     location = driver.find_elements_by_xpath("//dt[.='Location:']/following-sibling::dd[1]")
-        #     if not location: x = "None"
-        #     else: x = location[0].text
-        #     locations.append(x)
-            
-        #     #Get description
-            
-        #     description = driver.find_elements_by_xpath("/html/body/div[2]/div[3]/div/div/div/div/div[1]/div[2]/div[3]")
-        #     if not description: x = "None"
-        #     else: x = description[0].text
-        #     descriptions.append(x)
-            
-        #     #Go back to previous page 
-            
-        #     driver.back()
-            
-        #     #Add 1 to page counter- there should be 20 results scraped before the next page is accessed
-            
-        #     page_counter += 1
-        
-        # if page_counter == 20:
-            
-        #     #Get job ref 
-            
-        #     j_ref = driver.find_elements_by_xpath("//dt[.='Job Ref:']/following-sibling::dd")
-        #     for x in j_ref:
-        #         job_ref.append(x.text)
-            
-        #     #Get posted date
-            
-        #     posted_date = driver.find_elements_by_xpath("//dt[.='Posted:']/following-sibling::dd")
-        #     for x in posted_date:
-        #         posted_dates.append(x.text)
-            
-        #     #Get closing date 
-            
-        #     closing_date = driver.find_elements_by_xpath("//dt[.='Closing Date:']/following-sibling::dd")
-        #     for x in closing_date:
-        #         closing_dates.append(x.text)
-            
-        #     #Print out page counter to ensure there are 20 results scraped from every apge 
+        price = result.find_element_by_css_selector('span.currency-value').text
+        prices.append(price)
+    
+    print(shop_names)
+    print(is_ad)
+    print(titles)
+    print(star_ratings)
+    print(num_reviews)
+    print(prices)
 
-        #     print('Page Counter:' + str(page_counter))
-            
-        #     #Add 20 records to our overall record count, then reset page counter in anticipation of us clicking onto the next page 
-            
-        #     record_counter += 20
-        #     page_counter = 0
-            
-        #     #Print out our total records scraped, whether there's a value for each row, and how many pages we've scraped from the search results.
-            
-        #     print('Record Counter:' + str(record_counter))
-        #     print(len(employers) == record_counter)
-        #     print('Page ' + str(int(record_counter/20)) + ' scraping complete.')
-            
-        #     #Ensuring we're clicking on the right link to get to the next page - there are 3 separate ways its formatted across the search 
-            
-        #     if record_counter == 20:
-        #         num = 0
-        #     elif record_counter >= 120:
-        #         num = 6
-        #     else:
-        #         num = (record_counter/20)
+finally: 
                 
-        #     #Find and click on next link 
-            
-        #     next_page = driver.find_element_by_xpath("//ul[@class='pagination']//li[{}]".format(2+num))
-        #     next_page.click()
-
-    except:
-        break
-
-#Exit browser
-
-driver.quit()
-
-#Create dictionaries from our lists 
-
-# data = {}
-
-#Create dataframe from our dictionary  
-            
-# df = pd.DataFrame(data)
-
-#Save dataframe to a new CSV 
-
-# df.to_csv('raw_data.csv', index=False)
+    driver.quit()
