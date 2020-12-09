22b8f3e06b0e3d3a07fbdf4d4bde90b0f2bb3150
Anastasia Bizyayeva <70596760+anastasiabizyayeva@users.noreply.github.com>
2020-12-09 11:26:26 +0000

    updated comments for all python files

--- scraper_options.py
+++ scraper_options.py
@@ -7,6 +7,8 @@ Created on Tue Nov 24 15:03:54 2020
 # Set local path of chromedriver
 PATH = "C:/Program Files (x86)\chromedriver.exe"
 
-search_terms = ['nerdy greeting card']
+#Input the search terms you want to look up 
+search_terms = ['nerdy greeting card', 'birthday card', 'congratulations card']
 
-page_counter_limit = 200
+#Choose how many pages of each search term you want to scrape. Note that Etsy has a limit of 240 pages of search results for anything you look up, so that's the default here. 
+page_counter_limit = 240
